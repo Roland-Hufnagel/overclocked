d4bae6d1ca01d11fe6f3a3478c67a43986a9ba74
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-02-12 14:37:18 +0100

    add console.log

---
+++ components/MonthView/index.js
@@ -0,0 +1,11 @@
+import Placeholder from "../Placeholder";
+import WeekView from "../WeekView";
+console.log("Git Workflow");
+export default function MonthView() {
+  return (
+    <>
+      <Placeholder />
+      <WeekView />
+    </>
+  );
+}
